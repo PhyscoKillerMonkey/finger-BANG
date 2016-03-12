21cf36fc7a4be581a77cd1d048150da2ff56d49a
Reece Bennett <reece.bennett1@gmail.com>
2016-03-12 16:27:08 +0000

    Make bullets fire

---
+++ Assets/Shoot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public Rigidbody projectile;
+	public int speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) {
+			Debug.Log("Bang!");
+			Rigidbody clone = (Rigidbody) Instantiate (projectile, transform.position, transform.rotation);
+			clone.velocity = transform.TransformDirection (new Vector3 (0, 0, speed));
+			Destroy (clone.gameObject, 5);
+		}
+	}
+}
