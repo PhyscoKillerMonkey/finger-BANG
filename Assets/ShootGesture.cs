19399ecaee7f59c0dc8e9a26b14f2936489f3e9f
Reece Bennett <reece.bennett1@gmail.com>
2016-03-12 22:16:36 +0000

    Make gun fire bullets (not correct direction)

---
+++ Assets/ShootGesture.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Leap;
+
+public class ShootGesture : MonoBehaviour {
+
+	Controller c;
+	float oldPitch;
+	float maxChange;
+	public float threshold;
+	public Rigidbody projectile;
+	public int speed;
+
+	void Start ()
+	{
+		c = new Controller ();
+	}
+
+	void Update ()
+	{
+		foreach (Hand hand in c.Frame().Hands) {
+			float newPitch = hand.Direction.Pitch;
+			float change = oldPitch - newPitch;
+			maxChange = Mathf.Max (maxChange, change);
+			Debug.Log ("Change: " + (oldPitch - newPitch) + " Max: " + maxChange);
+			//Debug.Log(hand.Direction);
+			oldPitch = newPitch;
+
+			if (change > threshold) {
+				Rigidbody clone = (Rigidbody)Instantiate (projectile, transform.position, transform.rotation);
+				clone.velocity = transform.TransformDirection (new Vector3 (0, 0, speed));
+				Destroy (clone.gameObject, 5);
+			}
+		}
+	}
+}
