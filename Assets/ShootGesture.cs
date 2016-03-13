987d97a909ed5aa49948d5ed468ad7f0c9fe26e4
axk588 <axk588@cs.bham.ac.uk>
2016-03-13 10:36:30 +0000

    fdsgdsfdsaf

--- Assets/ShootGesture.cs
+++ Assets/ShootGesture.cs
@@ -14,6 +14,7 @@ public class ShootGesture : MonoBehaviour {
 	public int speedX;
 	public int speedY;
 	public int speedZ;
+	public bool isRight;
 
 	void Start () {
 		c = new Controller ();
@@ -21,20 +22,29 @@ public class ShootGesture : MonoBehaviour {
 
 	void Update () {
 		foreach (Hand hand in c.Frame().Hands) {
-			float newPitch = hand.Direction.Pitch;
-			float change = oldPitch - newPitch;
-			maxChange = Mathf.Max (maxChange, change);
-			//Debug.Log ("Change: " + (oldPitch - newPitch) + " Max: " + maxChange);
-			//Debug.Log(hand.Direction);
-			oldPitch = newPitch;
-			long now = c.Now ();
-			Debug.Log (now-lastFired);
-			if ((change > threshold || Input.GetButtonDown("Fire1")) && now - lastFired > fireRateMS) {
-				lastFired = now;
-				Rigidbody clone = (Rigidbody)Instantiate (projectile, transform.position, transform.rotation);
-				clone.velocity = transform.TransformDirection (new Vector3 (speedX, speedY, speedZ));
-				Destroy (clone.gameObject, 5);
+			if (hand.IsRight && isRight) {
+				fire (hand);
+			} else if (hand.IsLeft && !isRight) {
+				fire (hand);
 			}
+		} 
+	}
+
+	void fire(Hand hand) {
+		float newPitch = hand.Direction.Pitch;
+		float change = oldPitch - newPitch;
+		maxChange = Mathf.Max (maxChange, change);
+		//Debug.Log ("Change: " + (oldPitch - newPitch) + " Max: " + maxChange);
+		//Debug.Log(hand.Direction);
+		oldPitch = newPitch;
+		long now = c.Now ();
+		//Debug.Log (now - lastFired);
+		if ((change > threshold || Input.GetButtonDown ("Fire1")) && now - lastFired > fireRateMS) {
+			Debug.Log (change);
+			lastFired = now;
+			Rigidbody clone = (Rigidbody)Instantiate (projectile, transform.position, transform.rotation);
+			clone.velocity = transform.TransformDirection (new Vector3 (speedX, speedY, speedZ));
+			Destroy (clone.gameObject, 5);
 		}
 	}
 }
